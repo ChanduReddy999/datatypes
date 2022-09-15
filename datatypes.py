69dd50a578c0d5858d14d3d3f6b79b8beced9b2c
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-15 23:03:30 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+"""
+Day 1
+Today is thursday 15th sept 2022
+topic: Comments and Datatypes
+"""
+
+
+# print(type(1))
+# print(type(234))
+# print(type(6))
+# print(type(-2617546588196))
+# print(type(0))
+
+
+# print(type(.0))
+# print(type(0.67628))
+# print(type(-9.87498))
+# print(type(.7))
+# print(type(-.6557))
+
+
+# print(type('hello chandu'))
+# print(type("hello chandu"))
+# print(type('''hello chandu'''))
+# print(type("""hello chandu"""))
+
+
+# print('hello python')
+# print("hello chandu")
+# print('''chandu with python''')
+# print("""python guru""")
+
